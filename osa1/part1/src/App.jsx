1c0454c0c0b17dcdf026832a1f08b688d9ddc4bd
iippol <iippol@protonmail.com>
2026-04-03 23:54:26 +0300

    tehty reactin alkeet tehtävien alkuun asti

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -1,7 +1,47 @@
-const App = () => (
-	<div>
-		<p>Hello world</p>
-	</div>
-);
+const Hello = (props) => {
+	console.log(props);
+	return (
+		<div>
+			<p>
+				Hello {props.name}, you are {props.age} years old
+			</p>
+		</div>
+	);
+};
+
+const Footer = () => {
+	return (
+		<div>
+			greeting app created by
+			<a> ipe92</a>
+		</div>
+	);
+};
+
+const App = () => {
+	const nimi = "Pekka";
+	const ika = 10;
+
+	const friends = [
+		{ name: "Leevi", age: 4 },
+		{ name: "Venla", age: 10 },
+	];
+	console.log("Friends", friends);
+
+	return (
+		<>
+			<h1>Greetings</h1>
+			<Hello name="Maya" age={26 + 10} />
+			<Hello name={nimi} age={ika} />
+			<Footer />
+			<p>
+				{friends[0].name} {friends[0].age}
+			</p>
+			<p>
+				{friends[1].name} {friends[1].age}
+			</p>
+		</>
+	);
+};
 
 export default App;
