4f403d34d25408025d268a04480311df6422f3d1
iippol <iippol@protonmail.com>
2026-04-06 00:10:56 +0300

    1a tehtävät tehty

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -1,4 +1,25 @@
-import "./App.css";
+const Header = (props) => {
+	return <h1>{props.course}</h1>;
+};
+const Part = (props) => {
+	return (
+		<p>
+			{props.part} {props.exercise}
+		</p>
+	);
+};
+const Content = (props) => {
+	return (
+		<div>
+			<Part part={props.part1} exercises1={props.exercise} />
+			<Part part={props.part2} exercises2={props.exercise} />
+			<Part part={props.part3} exercises3={props.exercise} />
+		</div>
+	);
+};
+const Total = (props) => {
+	return <p>Number of exercises {props.exercises}</p>;
+};
 
 const App = () => {
 	const course = "Half Stack application development";
@@ -11,17 +32,16 @@ const App = () => {
 
 	return (
 		<div>
-			<h1>{course}</h1>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
-			<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+			<Header course={course} />
+			<Content
+				part1={part1}
+				exercises1={exercises1}
+				part2={part2}
+				exercises2={exercises2}
+				part3={part3}
+				exercises3={exercises3}
+			/>
+			<Total exercises={exercises1 + exercises2 + exercises3} />
 		</div>
 	);
 };
