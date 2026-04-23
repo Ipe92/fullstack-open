7220ff7907b14033d6a495972123443a3f4d3e87
ipe92 <iippol@protonmail.com>
2026-04-23 04:06:06 +0300

    unicafe step4 tehty

--- osa1/unicafe/src/App.jsx
+++ osa1/unicafe/src/App.jsx
@@ -3,17 +3,28 @@ import { useState } from "react";
 const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
 const Statistics = (props) => {
-	return (
-		<div>
-			<h1>statistics</h1>
-			<p>good {good}</p>
-			<p>neutral {neutral}</p>
-			<p>bad {bad}</p>
-			<p>all {good + bad + neutral}</p>
-			<p>average {calculateAverage()}</p>
-			<p>positive {calculatePositive()}%</p>
-		</div>
-	);
+	const feedBackAmount = props.good + props.bad + props.neutral;
+	const calculateAverage = () => (props.good - props.bad) / feedBackAmount;
+	const calculatePositive = () => props.good / feedBackAmount;
+	if (props.good === 0 && props.bad === 0 && props.neutral === 0) {
+		return (
+			<div>
+				<h1>statistics</h1>
+				<p>No feedback given</p>
+			</div>
+		);
+	} else
+		return (
+			<div>
+				<h1>statistics</h1>
+				<p>good {props.good}</p>
+				<p>neutral {props.neutral}</p>
+				<p>bad {props.bad}</p>
+				<p>all {props.good + props.bad + props.neutral}</p>
+				<p>average {calculateAverage()}</p>
+				<p>positive {calculatePositive()}%</p>
+			</div>
+		);
 };
 
 const App = () => {
@@ -22,16 +33,12 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-	// const feedBackAmount = good + bad + neutral;
-	// const calculateAverage = () => (good - bad) / feedBackAmount;
-	// const calculatePositive = () => good / feedBackAmount;
-
 	return (
 		<div>
+			<h1>Give feedback</h1>
 			<Button onClick={() => setGood(good + 1)} text="good" />
 			<Button onClick={() => setNeutral(neutral + 1)} text="neutral" />
 			<Button onClick={() => setBad(bad + 1)} text="bad" />
-			<h1>statistics</h1>
 			<Statistics good={good} neutral={neutral} bad={bad} />
 		</div>
 	);
